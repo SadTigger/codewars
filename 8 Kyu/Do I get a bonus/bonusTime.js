ff6c1cecb3e72199827bfe1433b8d23fd5a9b366
Pavel Zhachko <rammiel@yandex.ru>
2021-02-28 22:48:51 +0300

    feat: add 8 kyu kata solution: Do I get bonus?

---
+++ 8 Kyu/Do I get a bonus/bonusTime.js
@@ -0,0 +1,7 @@
+// Kata:
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+// Example:
+
+// Solution:
+const bonusTime = (salary, bonus) => bonus ? "\u00A3" + `${salary * 10}` : "\u00A3" + `${salary}`;
