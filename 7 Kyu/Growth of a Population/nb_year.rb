28fdd606ef6e2ea2767c5ac2a57e146f32e84796
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 02:18:43 +0300

    feat: add 7 kyu kata ruby solution: Growth of a Population

---
+++ 7 Kyu/Growth of a Population/nb_year.rb
@@ -0,0 +1,14 @@
+# Kata:
+# https://www.codewars.com/kata/563b662a59afc2b5120000c6
+
+# Example:
+
+# Solution:
+def nb_year(p0, percent, aug, p)
+    n = 0
+    while p0 < p 
+      p p0 = p0.to_i + p0.to_i * percent.to_f/100.0 + aug.to_i
+      n += 1
+    end
+    n
+end
