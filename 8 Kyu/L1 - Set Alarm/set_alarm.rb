72b30171445d204e40201b5ff6852b6002623c39
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 02:31:05 +0300

    feat: add 8 kyu kata ruby solution: L1 - Set Alarm

---
+++ 8 Kyu/L1 - Set Alarm/set_alarm.rb
@@ -0,0 +1,13 @@
+# Kata:
+# https://www.codewars.com/kata/568dcc3c7f12767a62000038
+
+# Example:
+
+# Solution:
+def set_alarm(employed, vacation)
+  if employed
+    vacation ? false : true
+  else 
+    return false
+  end
+end
