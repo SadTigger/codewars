2add15666de2d4322483623605de3bd9e22a95d6
Pavel Zhachko <rammiel@yandex.ru>
2021-03-01 10:14:04 +0300

    feat: add 7 kyu kata solution: SantaClausable Interface

---
+++ 7 Kyu/SantaClausable Interface/isSantaClausable.js
@@ -0,0 +1,7 @@
+// Kata:
+// https://www.codewars.com/kata/52b50a20fa0e77b304000103
+
+// Example:
+
+// Solution:
+const isSantaClausable = (obj) =>  typeof obj.sayHoHoHo  === 'function' && typeof obj.distributeGifts  === 'function' && typeof obj.goDownTheChimney  === 'function';
