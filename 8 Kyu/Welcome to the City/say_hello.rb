0396ccbdbf67d2d0003ce16c299b618a547a697d
Pavel Zhachko <rammiel@yandex.ru>
2021-03-01 11:04:54 +0300

    feat: add 8 kyu kata ruby solution: Welcome to the City

---
+++ 8 Kyu/Welcome to the City/say_hello.rb
@@ -0,0 +1,9 @@
+# Kata:
+# https://www.codewars.com/kata/5302d846be2a9189af0001e4
+
+# Example:
+
+# Solution:
+def say_hello(name, city, state)
+  "Hello, #{name.join(' ')}! Welcome to #{city}, #{state}!"# Your Code Here
+end
