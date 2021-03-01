463493f283d8ad9cf9e58b585abc9bcd6449cf6b
Pavel Zhachko <rammiel@yandex.ru>
2021-03-01 12:47:11 +0300

    feat: add 8 kyu kata ruby solution: Convert a Boolean to a String

---
+++ 8 Kyu/Convert a Boolean to a String/boolean_to_string.rb
@@ -0,0 +1,14 @@
+# Kata:
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+# Example:
+
+# Solution:
+def boolean_to_string(b)
+  case b
+  when true
+    "true"
+  when false
+    "false"
+  end
+end
