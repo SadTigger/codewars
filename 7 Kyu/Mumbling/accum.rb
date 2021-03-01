22a535778b1a97859696f95da9f560fccaebc3ac
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 02:49:58 +0300

    feat: add 7 kyu kata ruby solution: Mumbling

--- 7 Kyu/Mumbling/accum.rb
+++ 7 Kyu/Mumbling/accum.rb
@@ -1,6 +1,9 @@
 # Kata:
-# 
+# https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
 
 # Example:
 
 # Solution:
+def accum(s)
+  s.split(//).map.each_with_index { |letter, index| letter * (index +1) }.map { |i| i.capitalize }.join('-')
+end
