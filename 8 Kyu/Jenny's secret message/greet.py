b9bd4766b877444c2f730e340b2cfb8a22cf0c04
Pavel Zhachko <rammiel@yandex.ru>
2021-03-01 12:09:44 +0300

    feat: add 8 kyu kata python solution: Jenny's secret message

---
+++ 8 Kyu/Jenny's secret message/greet.py
@@ -0,0 +1,11 @@
+# Kata:
+# https://www.codewars.com/kata/55225023e1be1ec8bc000390
+
+# Example:
+
+# Solution:
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    else:
+        return "Hello, {name}!".format(name=name)
