9c21c02400b53e38b3d31c1898ff5a67ef6a9e80
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 11:58:38 +0300

    feat: add 8 kyu kata js solution: Fix your code before the garden dies!

---
+++ 8 Kyu/Fix your code before the garden dies!/rainAmount.js
@@ -0,0 +1,7 @@
+// Kata:
+// https://www.codewars.com/kata/57158fb92ad763bb180004e7
+
+// Example:
+
+// Solution:
+const rainAmount = (mm) => mm >= 40 ? "Your plant has had more than enough water for today!" : `You need to give your plant ${ 40 - mm }mm of water`;
