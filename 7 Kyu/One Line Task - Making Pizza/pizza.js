8c31b2d34dbf618d623bbf12018476ffea4df675
Pavel Zhachko <rammiel@yandex.ru>
2021-03-03 02:19:49 +0300

    feat: add 7 kyu kata js solution: One Line Task: Making Pizza

---
+++ 7 Kyu/One Line Task - Making Pizza/pizza.js
@@ -0,0 +1,23 @@
+// Kata:
+// https://www.codewars.com/kata/58dbb9b82f449e134a000119
+
+// Example:
+
+// Solution:
+function makePizza(pieces) { //Let's make n pieces of Pizza ;-)
+  var result = "";
+  while ( pieces-- ){
+    var needSteps = 5;
+    while ( needSteps-- ) result += make( needSteps +1);
+  }
+  return result;
+}
+function make(step){
+  switch ( step ){
+    case 5: return "P";
+    case 4: return "i";
+    case 3: 
+    case 2: return "z";
+    case 1: return "a";
+  }
+}
