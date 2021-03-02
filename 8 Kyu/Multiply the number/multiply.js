361626e759b4bc48dcffe968723f12dd72f12e03
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 11:50:59 +0300

    feat: add 8 kyu kata js solution: Multiply the number

---
+++ 8 Kyu/Multiply the number/multiply.js
@@ -0,0 +1,7 @@
+// Kata:
+// https://www.codewars.com/kata/5708f682c69b48047b000e07
+
+// Example:
+
+// Solution:
+const multiply = (number) => number * Math.pow(5, Math.abs(number).toString().length );
