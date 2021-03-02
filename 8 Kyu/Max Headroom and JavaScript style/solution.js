75134b4bd36e81f1378177cc8c519cc8a7b49277
Pavel Zhachko <rammiel@yandex.ru>
2021-03-03 02:03:01 +0300

    feat: add 8 kyu kata js solution: Max Headroom and JavaScript style

---
+++ 8 Kyu/Max Headroom and JavaScript style/solution.js
@@ -0,0 +1,17 @@
+// Kata:
+// https://www.codewars.com/kata/52a47dd7e950edabfa000391
+
+// Example:
+
+// Solution:
+const getMax1 = () => {
+    return {
+        name: 'Max Headroom'
+    };
+  }
+  
+const getMax2 = () => {
+    return {
+        name: 'Max Headroom'
+    }
+}
