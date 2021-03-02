a98b4d2e7c86f4ad0bdf390936caf8acc518dd90
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 03:17:52 +0300

    feat: add 8 kyu kata js solution: Count the Monkeys!

---
+++ 8 Kyu/Count the Monkeys!/monkeyCount.js
@@ -0,0 +1,13 @@
+// Kata:
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+// Example:
+
+// Solution:
+const monkeyCount = (n) => {
+  const arr = [];
+  for (let x = 1; x <= n; x++) {
+    arr.push(x);
+  }
+  return arr;
+}
