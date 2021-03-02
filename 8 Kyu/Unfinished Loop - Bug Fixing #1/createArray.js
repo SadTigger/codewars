f5cb837354654c378857afc8e38fdc8686441a90
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 11:55:28 +0300

    feat: add 8 kyu kata js solution: Unfinished Loop - Bug Fixing #1

---
+++ 8 Kyu/Unfinished Loop - Bug Fixing #1/createArray.js
@@ -0,0 +1,15 @@
+// Kata:
+// 
+
+// Example:
+
+// Solution:
+const createArray = (number) => {
+  const newArray = [];
+  
+  for(let counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
