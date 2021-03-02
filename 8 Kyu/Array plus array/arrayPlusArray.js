181baab2b9481b890df05abee11dc9e17905d542
Pavel Zhachko <rammiel@yandex.ru>
2021-03-02 11:34:46 +0300

    feat: add 8 kyu kata js solution: Array plus array

---
+++ 8 Kyu/Array plus array/arrayPlusArray.js
@@ -0,0 +1,7 @@
+// Kata:
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+// Example:
+
+// Solution:
+const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((a, b) => a + b);
